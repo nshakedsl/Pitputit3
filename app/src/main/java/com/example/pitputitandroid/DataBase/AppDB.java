43accd1098dbd0b11a440818787ec8a4602e8331
OfirGurvits <ofirgurvits@gmail.com>
2023-06-26 10:06:50 +0300

    db works?

--- app/src/main/java/com/example/pitputitandroid/DataBase/AppDB.java
+++ app/src/main/java/com/example/pitputitandroid/DataBase/AppDB.java
@@ -5,14 +5,16 @@ import androidx.room.Database;
 import androidx.room.DatabaseConfiguration;
 import androidx.room.InvalidationTracker;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
+import com.example.pitputitandroid.Converters.LastMessageConverter;
+import com.example.pitputitandroid.Converters.UserConverter;
 import com.example.pitputitandroid.Daos.ChatDao;
 import com.example.pitputitandroid.entities.Chat;
-/*
+
+@TypeConverters({LastMessageConverter.class, UserConverter.class})
 @Database(entities = {Chat.class}, version = 1)
 public abstract class AppDB extends RoomDatabase{
     public abstract ChatDao chatDao();
-
 }
-*/
