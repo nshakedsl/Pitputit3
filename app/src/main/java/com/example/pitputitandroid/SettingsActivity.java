7548c09c0d7173b9b22380cc3e3055c0088538dd
OfirGurvits <ofirgurvits@gmail.com>
2023-06-27 23:05:08 +0300

    added log out

--- app/src/main/java/com/example/pitputitandroid/SettingsActivity.java
+++ app/src/main/java/com/example/pitputitandroid/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.pitputitandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,22 +15,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageView;
+
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 
+import com.example.pitputitandroid.DataBase.AppDB;
 import com.example.pitputitandroid.R;
 import com.example.pitputitandroid.RegisterActivity;
+import com.example.pitputitandroid.entities.Message;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class SettingsActivity extends AppCompatActivity {
 
-     Switch darkModeSwitch;
+    Switch darkModeSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-
+        Button logoutButton = findViewById(R.id.logOut);
+        logoutButton.setOnClickListener(v -> logOut());
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.hide();
@@ -41,68 +48,37 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
 
-        darkModeSwitch=findViewById(R.id.dark_mode_switch);
-        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()  {
+        darkModeSwitch = findViewById(R.id.dark_mode_switch);
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged( CompoundButton compoundButton, boolean b) {
-                if(b){
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                }
-                else{
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
             }
         });
 
-//        darkModeSwitch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // Handle the click event here
-//                boolean isChecked = ((CompoundButton) view).isChecked();
-//                if (isChecked) {
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-//                } else {
-//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-//                }
-//            }
-//        });
+    }
 
+    public void logOut() {
+        //todo shaked: clear prefrences
+        AppDB db = AppDB.getInstance(this);
+        db.chatDao();
+        //creates a thread that clears the messages
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                db.chatDao().clearChats();
+                db.messageDao().clearMessages();
+            }
+        });
 
-
-//        AppCompatButton updateButton = findViewById(R.id.updateButton);
-//        EditText serverIpText = findViewById(R.id.serverIpInput);
-//        ImageView imglogo = findViewById(R.id.imageLogo);
-//        TextView settingsText = findViewById(R.id.textSettings);
-//
-//        darkModeSwitch = findViewById(R.id.dark_mode_switch);
-//
-//        darkModeSwitch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (darkModeSwitch.isChecked()) {
-//                    // Switch to dark mode
-//                    mainLayout.setBackgroundColor(getResources().getColor(android.R.color.background_dark));
-//                } else {
-//                    // Switch to light mode
-//                    mainLayout.setBackgroundColor(getResources().getColor(android.R.color.background_light));
-//                }
-//            }
-//        });
-//
-//
-//        updateButton.setOnClickListener(v ->
-//                loginClick(serverIpText.getText()));
-//        //sends to register/signup page
-//        registerText.setOnClickListener(v -> {
-//            startActivity(new Intent(this, RegisterActivity.class));
-//        });
-//
-//
-//        updateButton.animate().translationY(50).setDuration(700).setStartDelay(0);
-//        serverIpText.animate().translationY(50).setDuration(700).setStartDelay(0);
-//        imglogo.animate().translationY(50).setDuration(700).setStartDelay(0);
-//        settingsText.animate().translationY(50).setDuration(700).setStartDelay(0);
-
-
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
