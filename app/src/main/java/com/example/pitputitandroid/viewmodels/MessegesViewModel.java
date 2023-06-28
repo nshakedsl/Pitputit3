b803d437cf44d6400df9edbeaa41bef35b94d0c6
OfirGurvits <ofirgurvits@gmail.com>
2023-06-28 23:33:41 +0300

    temp

--- app/src/main/java/com/example/pitputitandroid/viewmodels/MessegesViewModel.java
+++ app/src/main/java/com/example/pitputitandroid/viewmodels/MessegesViewModel.java
@@ -5,19 +5,21 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.pitputitandroid.Repositories.ChatsRepository;
+import com.example.pitputitandroid.Repositories.MarkedMessageRepository;
 import com.example.pitputitandroid.Repositories.MessageRepository;
 import com.example.pitputitandroid.entities.Chat;
+import com.example.pitputitandroid.entities.MarkedMessage;
 import com.example.pitputitandroid.entities.Message;
 
 import java.util.List;
 
 
 public class MessegesViewModel extends ViewModel {
-    private MessageRepository messageRepository;
+    private MarkedMessageRepository messageRepository;
     private LiveData<List<Message>> messages;
 
-    public MessegesViewModel() {
-        this.messageRepository = new MessageRepository();
+    public MessegesViewModel(String origin) {
+        this.messageRepository = new MarkedMessageRepository(origin);
         messages = messageRepository.getAll();
     }
 
